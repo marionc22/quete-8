b7d4e77a687afa8028f24d98e284b57830d1bee0
marionc22 <103053810+marionc22@users.noreply.github.com>
2022-11-07 17:09:00 +0100

    Add files via upload

---
+++ quest-java-oop3-master/Nature.java
@@ -0,0 +1,18 @@
+public class Nature {
+
+    public static void main(String[] args) {
+
+
+
+        Eagle hawkeye = new Eagle("Hawkeye");
+        // TODO : uncomment the following code in order to test it
+       hawkeye.takeOff();
+        hawkeye.ascend(120);
+        hawkeye.ascend(30);
+       hawkeye.glide();
+        hawkeye.descend(140);
+        hawkeye.land();
+       hawkeye.descend(9);
+       hawkeye.land();
+    }
+}
